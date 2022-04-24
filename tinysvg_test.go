48a01660c563705efd875b96cdf8b29424e4f039
Alexander F. Rødseth <rodseth@gmail.com>
2022-04-24 23:36:21 +0200

    Add two tests that fail

--- tinysvg_test.go
+++ tinysvg_test.go
@@ -2,34 +2,47 @@ package tinysvg
 
 import (
 	"bytes"
-	"fmt"
 	"testing"
 )
 
 func TestSVG(t *testing.T) {
 	document, svg := NewTinySVG(256, 256)
 	svg.Describe("Diagram")
-
 	roundedRectangle := svg.AddRoundedRect(30, 10, 5, 5, 20, 20)
 	roundedRectangle.Fill("red")
-
 	document.SaveSVG("/tmp/output.svg")
+}
+
+func TestString(t *testing.T) {
+	document, svg := NewTinySVG(256, 256)
+	svg.Describe("Diagram")
+	roundedRectangle := svg.AddRoundedRect(30, 10, 5, 5, 20, 20)
+	roundedRectangle.Fill("red")
+	s := document.String()
+	if len(s) != 258 {
+		t.Fatalf("1: length is not 258 but %d\n", len(s))
+	}
+	s = document.String()
+	if len(s) != 258 {
+		t.Fatalf("2: length is not 258 but %d\n", len(s))
+	}
 }
 
 func TestWriteTo(t *testing.T) {
 	document, svg := NewTinySVG(256, 256)
 	svg.Describe("Diagram")
-
 	roundedRectangle := svg.AddRoundedRect(30, 10, 5, 5, 20, 20)
 	roundedRectangle.Fill("red")
-
 	var buf bytes.Buffer
-	if _, err := document.WriteTo(&buf); err != nil {
-		t.Error(err)
+	document.WriteTo(&buf)
+	s := buf.String()
+	if len(s) != 258 {
+		t.Fatalf("1: length is not 258 but %d\n", len(s))
 	}
-
-	if buf.String() != document.String() {
-		t.Fail()
+	var buf2 bytes.Buffer
+	document.WriteTo(&buf2)
+	s = buf2.String()
+	if len(s) != 258 {
+		t.Fatalf("2: length is not 258 but %d\n", len(s))
 	}
-
 }
