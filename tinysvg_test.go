ee7e29b3f29c465e8408d590c3508dc2f8a9fb6f
Alexander F. Rødseth <rodseth@gmail.com>
2022-04-24 23:16:36 +0200

    Add a test that fails

--- tinysvg_test.go
+++ tinysvg_test.go
@@ -1,6 +1,8 @@
 package tinysvg
 
 import (
+	"bytes"
+	"fmt"
 	"testing"
 )
 
@@ -12,4 +14,22 @@ func TestSVG(t *testing.T) {
 	roundedRectangle.Fill("red")
 
 	document.SaveSVG("/tmp/output.svg")
+}
+
+func TestWriteTo(t *testing.T) {
+	document, svg := NewTinySVG(256, 256)
+	svg.Describe("Diagram")
+
+	roundedRectangle := svg.AddRoundedRect(30, 10, 5, 5, 20, 20)
+	roundedRectangle.Fill("red")
+
+	var buf bytes.Buffer
+	if _, err := document.WriteTo(&buf); err != nil {
+		t.Error(err)
+	}
+
+	if buf.String() != document.String() {
+		t.Fail()
+	}
+
 }
